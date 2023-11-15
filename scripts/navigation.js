abc0994bcab2aaa4232e0c7a6074a1e269110e78
Ingrid Gruener <142811068+ingridgruenerluft@users.noreply.github.com>
2023-11-15 21:27:32 +0000

    newsletter

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,24 +1,3 @@
-$('#button1').on('click', function() {
-    window.location.href = "https://www.washingtonpost.com/business/2020/10/08/jobless-claims-inched-downward-840000-last-week-pandemic-continues-weigh-labor-market/";
+$('#subscribe').on('click', function(event) {
+    href = 'https://cornell.us15.list-manage.com/subscribe?id=63fa7e7323&u=95184ae5551472d32c2d92686';
 });
-
-$('#button2').on('click', function() {
-    window.location.href = "https://www.wsj.com/articles/u-s-budget-gap-tripled-in-fiscal-2020-as-government-battled-pandemic-11602185589";
-});
-
-$('#button3').on('click', function() {
-    window.location.href = "https://www.reuters.com/article/usa-banks-trading/focus-treasuries-on-steroids-u-s-banks-mortgage-bond-trading-bonanza-idUSL1N2GY2B3";
-});
-
-$('#button4').on('click', function() {
-    window.location.href = "https://www.cnbc.com/2020/10/09/uk-gdp-august-2020.html";
-});
-
-$('#button5').on('click', function() {
-    window.location.href = "https://www.nytimes.com/2020/10/09/us/politics/trump-covid-stimulus-pelosi-republicans.html";
-});
-
-$('#button6').on('click', function() {
-    window.location.href = "https://www.federalreserve.gov/newsevents/speech/powell20201006a.htm";
-});
-
