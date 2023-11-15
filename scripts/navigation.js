52693ba6a5f7ae11b2a46d1cc6bb5ac66f3286b3
Ingrid Gruener <142811068+ingridgruenerluft@users.noreply.github.com>
2023-11-15 18:05:39 +0000

    navigation page

---
+++ scripts/navigation.js
@@ -0,0 +1,23 @@
+document.getElementById("button1").onclick = function () {
+    href = "https://www.washingtonpost.com/business/2020/10/08/jobless-claims-inched-downward-840000-last-week-pandemic-continues-weigh-labor-market/";
+};
+
+document.getElementById("button2").onclick = function () {
+    href = "https://www.wsj.com/articles/u-s-budget-gap-tripled-in-fiscal-2020-as-government-battled-pandemic-11602185589";
+};
+
+document.getElementById("button3").onclick = function () {
+    href = "https://www.reuters.com/article/usa-banks-trading/focus-treasuries-on-steroids-u-s-banks-mortgage-bond-trading-bonanza-idUSL1N2GY2B3";
+};
+
+document.getElementById("button4").onclick = function () {
+    href = "https://www.cnbc.com/2020/10/09/uk-gdp-august-2020.html";
+};
+
+document.getElementById("button5").onclick = function () {
+    href = "https://www.nytimes.com/2020/10/09/us/politics/trump-covid-stimulus-pelosi-republicans.html";
+};
+
+document.getElementById("button6").onclick = function () {
+    href = "https://www.federalreserve.gov/newsevents/speech/powell20201006a.htm";
+};
