56891f0d6200d47ed327c7abc332924f93a54c92
elainewucu <142811215+elainewucu@users.noreply.github.com>
2023-11-29 20:59:21 +0000

    Fixed image carousel buttons functionality

--- scripts/chief-editor-carousel.js
+++ scripts/chief-editor-carousel.js
@@ -8,22 +8,22 @@ setInterval(function() {
       }
   }, 7000);
 
-$("#left-arrow-button").click(function() {
-  if ($("#joel-pang").hasClass("hidden")) {
-    $("#joel-pang").removeClass("hidden");
-    $("#noah-cordon-siskind").addClass("hidden");
-  } else {
-    $("#joel-pang").addClass("hidden");
-    $("#noah-cordon-siskind").removeClass("hidden");
-  }
+$("#right-arrow-button-j").click(function() {
+  $("#joel-pang").addClass("hidden");
+  $("#noah-cordon-siskind").removeClass("hidden");
 });
 
-$("#right-arrow-button").click(function() {
-  if ($("#joel-pang").hasClass("hidden")) {
-    $("#joel-pang").removeClass("hidden");
-    $("#noah-cordon-siskind").addClass("hidden");
-  } else {
-    $("#joel-pang").addClass("hidden");
-    $("#noah-cordon-siskind").removeClass("hidden");
-  }
+$("#left-arrow-button-j").click(function() {
+  $("#joel-pang").addClass("hidden");
+  $("#noah-cordon-siskind").removeClass("hidden");
+});
+
+$("#right-arrow-button-n").click(function() {
+  $("#joel-pang").removeClass("hidden");
+  $("#noah-cordon-siskind").addClass("hidden");
+});
+
+$("#left-arrow-button-n").click(function() {
+  $("#joel-pang").removeClass("hidden");
+  $("#noah-cordon-siskind").addClass("hidden");
 });
